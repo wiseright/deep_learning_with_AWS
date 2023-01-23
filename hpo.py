910109ed526e86e0d37a3a80ce7c18309c577102
wiseright <allencavaliereoscuro@gmail.com>
2023-01-23 07:04:35 +0100

    Completed hpo.py and tuner definition

--- hpo.py
+++ hpo.py
@@ -7,8 +7,21 @@ import torch.optim as optim
 import torchvision
 import torchvision.models as models
 import torchvision.transforms as transforms
+import torchvision.transforms as T
+import webdataset as wds
 
 import argparse
+
+# Definisce una map su ogni elemento Resize immagine
+def preprocess(sample):
+    image, cls = sample
+    #image = T.Resize(size=(224,224))(T.ToTensor()(image))
+    image = T.ToTensor()(image)
+    image = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(image)
+    image = T.Resize(size=(224,224))(image)
+    label = cls
+    
+    return 1-image, label
 
 def test(model, test_loader):
     '''
@@ -16,7 +29,22 @@ def test(model, test_loader):
           testing data loader and will get the test accuray/loss of the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print(
+        "\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss, correct, len(test_loader.dataset), 100.0 * correct / len(test_loader.dataset)
+        )
+    )
 
 def train(model, train_loader, criterion, optimizer):
     '''
@@ -24,7 +52,22 @@ def train(model, train_loader, criterion, optimizer):
           data loaders for training and will get train the model
           Remember to include any debugging/profiling hooks that you might need
     '''
-    pass
+    for batch_idx, (data, target) in enumerate(train_loader):
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 100 == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
     
 def net():
     '''
@@ -39,14 +82,25 @@ def net():
                    nn.Linear(num_features, 133))
     return model
 
-def create_data_loaders(data, batch_size):
+def create_loader(url, batch_size):
     '''
     This is an optional function that you may or may not need to implement
     depending on whether you need to use data loaders or not
     '''
-    pass
+    # Create Dataset from tar archive
+    dataset = wds.WebDataset(url).shuffle(1000).decode("rgb").to_tuple("jpg.input.jpg", "jpg.target.cls").map(preprocess)
+    dataset = dataset.batched(16)
+    
+    # Create Loader
+    loader = wds.WebLoader(dataset, num_workers=2, batch_size=None)
+    loader = loader.unbatched().shuffle(1000).batched(batch_size)
+    
+    return loader
+
 
 def main(args):
+    print(args)
+    
     '''
     TODO: Initialize a model by calling the net function
     '''
@@ -56,23 +110,34 @@ def main(args):
     TODO: Create your loss and optimizer
     '''
     loss_criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.fc.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.lr)
     
     '''
     TODO: Call the train function to start training your model
     Remember that you will need to set up a way to get training data from S3
-    '''
+    '''      
+    train_loader = create_loader(url = 's3://sagemaker-us-east-1-265345480326/project_3/dogImages_dataset.tar',
+                                 batch_size=args.batch_size)
+    
     model=train(model, train_loader, loss_criterion, optimizer)
     
     '''
     TODO: Test the model to see its accuracy
     '''
+    train_loader = create_loader(url = 's3://sagemaker-us-east-1-265345480326/project_3/dogImages_dataset.tar',
+                                 batch_size=args.test_batch_size)
+    
     test(model, test_loader, criterion)
+    
+    # training and testing
+    for epoch in range(1, args.epochs + 1):
+        train(model, train_loader, optimizer, epoch)
+        test(model, test_loader)
     
     '''
     TODO: Save the trained model
     '''
-    torch.save(model, path)
+    torch.save(model, './model')
 
    
 if __name__=='__main__':
